9c0235c07b8c3742950c3a273c9ac18265814969
HadiChaaban <chaabanh@umich.edu>
2022-11-30 09:38:19 -0500

    Fixed Integration Issues

--- Assets/Photon/PhotonUnityNetworking/Code/PhotonNetworkPart.cs
+++ Assets/Photon/PhotonUnityNetworking/Code/PhotonNetworkPart.cs
@@ -2509,7 +2509,6 @@ namespace Photon.Pun
 
         // to be used in the main thread. as OnRegionsPinged is called in a separate thread and so we can't use some of the Unity methods (like saving playerPrefs)
         private static RegionHandler _cachedRegionHandler;
-        public static object player;
 
         private static void OnRegionsPinged(RegionHandler regionHandler)
         {
