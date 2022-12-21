9529702957d59bfc2e6fa7c181182859ce3f636a
JavaJimmy10 <majedahmad03@hotmail.com>
2022-12-21 14:52:45 -0500

    Shooting code has comments

--- Assets/Scripts/Shooting Scripts/GunScript.cs
+++ Assets/Scripts/Shooting Scripts/GunScript.cs
@@ -18,34 +18,34 @@ public class GunScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        PointToMouse(ref angle);
+        PointToMouse();
         shoot();
     }
 
-    void PointToMouse(ref float angle)
+    void PointToMouse()
     {
-        Vector2 mousePos = Input.mousePosition;
+        Vector2 mousePos = Input.mousePosition; //Gets mouse position
         Vector3 gunPos = Camera.main.WorldToScreenPoint(this.gameObject.transform.position);
 
-        float sideA = mousePos.x - gunPos.x;
-        float sideB = mousePos.y - gunPos.y;
+        float sideA = mousePos.x - gunPos.x; //One leg of triangle between gun and mouse
+        float sideB = mousePos.y - gunPos.y; //One leg of triangle between gun and mouse
 
-        angle = Mathf.Atan2(sideB, sideA) * Mathf.Rad2Deg;
+        angle = Mathf.Atan2(sideB, sideA) * Mathf.Rad2Deg; //Gets angle between two points
 
-        this.gameObject.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+        this.gameObject.transform.rotation = Quaternion.Euler(0f, 0f, angle); //Sets rotation to the angle
     }
 
     void shoot()
     {
-        RaycastHit hit;
+        RaycastHit hit; //The raycast collision
 
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.DrawRay(shootPoint.transform.position, transform.TransformDirection(Vector3.right) * 10, Color.red);
+            Debug.DrawRay(shootPoint.transform.position, transform.TransformDirection(Vector3.right) * 10, Color.red); //Draws a ray to visualize raycast
 
-            if (Physics.Raycast(shootPoint.transform.position, transform.TransformDirection(Vector3.right) * 10, out hit))
+            if (Physics.Raycast(shootPoint.transform.position, transform.TransformDirection(Vector3.right) * 10, out hit)) //Detects if raycast hit an object and if so stores the object in hit
             {
-                GameObject hitObject = hit.transform.gameObject;
+                GameObject hitObject = hit.transform.gameObject; //Stores hit object in variable
 
                 Debug.Log(hitObject.name);
             }
